763a40b2363c229848031a21ff0b7872049017b2
stryc <strycharz.mariusz84@gmail.com>
2022-02-24 01:07:41 +0100

    dodane początki menu

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -4,25 +4,60 @@ import java.util.Set;
 
 public class Hangman {
     public void start() {
-        Player player1 = new Player();
-        Player player2 = new Player();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("select second players name");
-        player1.setName(scanner.next());
-        player1.setChances(7);
-        player1.setScore(0);
+        System.out.println("HangMan - Wisielec");
+        System.out.println("Proszę wybrać opcję z menu:\n" +
+                "1. Nowa gra\n" +
+                "2. Statystyki\n" +
+                "3. Dodaj słowo\n" +
+                "4. Koniec\n");
+        System.out.print("Twój wybór: ");
+        int choiceMenu = scanner.nextInt();
+        Player player1 = new Player();
+        //Player player2 = new Player();
+        switch (choiceMenu) {
+            case 1:
+                System.out.print("Podaj swoje imię: ");
+                player1.setName(scanner.next());
+                player1.setChances(7);
+                player1.setScore(0);
 
 //        System.out.println("select second players name");
 //        player2.setName(scanner.next());
 //        player2.setChances(7);
 //        player2.setScore(0);
 
-        runRuond(player1,scanner);
+                runRound(player1, scanner);
 
+                break;
+            case 2:
+                System.out.println("HangMan - Wisielec\n" +
+                        "    Statystyki");
+                System.out.println("Proszę wybrać opcję z menu:\n" +
+                        "1. Top 10 graczy\n" +
+                        "2. Najczęściej wykorzystywane słowa\n" +
+                        "3. ........\n" +
+                        "4. Powrót\n");
+                System.out.print("Twój wybór: ");
+                int choiceMenuStats = scanner.nextInt();
+                switch (choiceMenuStats) {
+                    case 1:
+                        System.out.println("Top 10 graczy");
+                        break;
+                    case 2:
+                        System.out.println("Najczęściej wykorzystywane słowa");
+                        break;
+                    case 3:
+                        System.out.println("WORK IN PROGRESS!");
+                        break;
+                    case 4:
+                        break;
+                }
+        }
     }
 
-    public static void runRuond(Player player,Scanner scanner){
+    public static void runRound(Player player,Scanner scanner){
         Set<Character> letters = new HashSet<>();
 
         WordProccessor wordProccessor = new WordProccessor();
